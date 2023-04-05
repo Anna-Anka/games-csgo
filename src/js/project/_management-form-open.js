0d76b5818dd723e87176d442ba5a59ec614a21eb
Anna-Anka <anjbekrenewa@gmail.com>
2023-04-05 16:55:11 +0300

    added history-bet

--- src/js/project/_management-form-open.js
+++ src/js/project/_management-form-open.js
@@ -5,7 +5,6 @@ export const openManagementform = () => {
     if (document.querySelector('.management-form__form') && document.querySelector('.management-form__open')) {
         const button = document.querySelector('.management-form__open');
         const management = document.querySelector('.management-form');
-        const overlay = document.querySelector('.overlay');
 
         const checkClass = () => {
             if (management.classList.contains('management-form--active')) {
@@ -24,16 +23,6 @@ export const openManagementform = () => {
             management.classList.toggle('management-form--active');
             overlay.classList.toggle('overlay--active');
             checkClass();
-        })
-
-        overlay.addEventListener('click', (e) => {
-            if (!management.contains(e.target)) {
-                management.classList.remove('management-form--active');
-                overlay.classList.remove('overlay--active');
-                button.setAttribute('aria-expanded', 'false');
-                button.setAttribute('aria-label', 'Открыть фильтр');
-                enableScroll();
-            }
         })
     }
 }
